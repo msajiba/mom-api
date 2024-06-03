0995beb7d8f1c9b39cde25ae0130bdbb26241079
msajiba <asajib499@gmail.com>
2024-06-03 15:32:09 +0600

    modify pages

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,6 @@ export default function App({ Component, pageProps }) {
     <>
       <nav style={{ margin: "50px 0px" }}>
         <ul style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
-          {/* <li
-            style={{
-              cursor: "pointer",
-              border: "1px solid gray",
-              padding: "10px",
-            }}
-          >
-            <Link href={"/compress-pdf"}> Compress Pdf </Link>
-          </li> */}
           <li
             style={{
               cursor: "pointer",
@@ -22,7 +13,16 @@ export default function App({ Component, pageProps }) {
               padding: "10px",
             }}
           >
-            <Link href={"/remove-pdf-pages"}> Remove Pdf Pages </Link>{" "}
+            <Link href={"/pdf-to-word"}> PDF to Word </Link>
+          </li>
+          <li
+            style={{
+              cursor: "pointer",
+              border: "1px solid gray",
+              padding: "10px",
+            }}
+          >
+            <Link href={"/remove-pdf-pages"}> Remove Pdf Pages </Link>
           </li>
         </ul>
       </nav>
